470816a4f012f1f51fe08367de98c9648f3b9ffd
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-14 22:10:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,7 +15,15 @@ def find_max_value(array)
   end
 end
 
-#def find_min_value(array)
- # array.sort
-  #return array.first
-#end
+def find_min_value(array)
+  counter = 0 
+  while counter < array.length do 
+    while (array).all? {|value| array[counter]<=value} do 
+      return array[counter]
+    end
+    counter = counter + 1 
+  end
+end
+
+  return array.first
+end
