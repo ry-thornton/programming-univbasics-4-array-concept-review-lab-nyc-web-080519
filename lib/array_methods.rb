23fa1ae5dbe52f6d6f66a078c46f34e1a8434ceb
ryanthornton628 <ryanthornton628@gmail.com>
2019-07-14 21:49:52 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,8 +7,8 @@ end
 
 def find_max_value(array)
   counter = 0
-  def while counter < array.length do
-    def while all? {|array| array[counter]>=array[index]} do 
+  while counter < array.length do
+    while all? {|array| array[counter]>=array[index]} do 
       return array[counter]
     end
     counter = counter + 1 
